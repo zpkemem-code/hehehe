a472db0d98cfa7368b1d2f0dbaa32e62b80b7765
zpcode <zpkemem@gmail.com>
2026-08-03 20:31:12 +0800

    Update update_command.py

--- command/update_command.py
+++ command/update_command.py
@@ -11,6 +11,7 @@ from time import perf_counter
 from typing import Any, Dict
 from uuid import uuid4
 
+import pyrogram
 import pyrogram_styled
 import pyrogram_styled.enums
 import pyrogram_styled.errors
