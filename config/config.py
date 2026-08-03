f5d6569f0b06763a3c64c88ac68e7d216df22f21
zpcode <zpkemem@gmail.com>
2026-08-03 10:05:00 +0800

    Update environment variables and default values

--- config/config.py
+++ config/config.py
@@ -27,14 +27,14 @@ COPY_ID = {}
 
 IS_JASA_PRIVATE = os.environ.get("IS_JASA_PRIVATE", False)
 IS_CURI_DATA = os.environ.get("IS_CURI_DATA", True)
-WAJIB_JOIN = list(os.environ.get("WAJIB_JOIN", "aboutalldyz").split())
-USENAME_OWNER = os.environ.get("USENAME_OWNER", "@NDYSUPPORT")
-API_ID = int(os.environ.get("API_ID", 3617))
-MAX_BOT = int(os.environ.get("MAX_BOT", 2000))
-API_HASH = os.environ.get("API_HASH", "157c923aaf2b5db02788b381")
+WAJIB_JOIN = list(os.environ.get("WAJIB_JOIN", "zpsexz").split())
+USENAME_OWNER = os.environ.get("USENAME_OWNER", "@iscrtz")
+API_ID = int(os.environ.get("API_ID", 31019298))
+MAX_BOT = int(os.environ.get("MAX_BOT", 10))
+API_HASH = os.environ.get("API_HASH", "f80a208b8cd4709c30c26ceacae9e1be")
 
 BOT_TOKEN = os.environ.get(
-    "BOT_TOKEN", "8507904900:AAFfyr5OdyiYL9E6dzbdizBqRQiglL9gGr0"
+    "BOT_TOKEN", "8957183252:AAF4Em7_hBMmPAvuKqZwqPI395fUvw_iVZE"
 )
 
 API_KEY = os.environ.get("API_KEY", "dp_35ks7liix6")
@@ -42,9 +42,9 @@ API_KEY = os.environ.get("API_KEY", "dp_35ks7liix6")
 BOT_ID = int(BOT_TOKEN.split(":")[0])
 API_MAELYN = os.environ.get("API_MAELYN", "kenapanan")
 
-BOT_NAME = os.environ.get("BOT_NAME", "JASEB-AUTO UBOT")
+BOT_NAME = os.environ.get("BOT_NAME", "UserbotSuki")
 
-DB_NAME = os.environ.get("DB_NAME", "JASEB-AUTO UBOT")
+DB_NAME = os.environ.get("DB_NAME", "starzp")
 
 URL_LOGO = os.environ.get("URL_LOGO", "https://files.catbox.moe/knaxi2.jpg")
 
@@ -55,7 +55,7 @@ SUDO_OWNERS = list(
         int,
         os.environ.get(
             "SUDO_OWNERS",
-            "7230983425",
+            "7586938131",
         ).split(),
     )
 )
@@ -64,27 +64,27 @@ DEVS = list(
         int,
         os.environ.get(
             "DEVS",
-            "7230983425",
+            "7586938131",
         ).split(),
     )
 )
 
 AKSES_DEPLOY = list(
-    map(int, os.environ.get("AKSES_DEPLOY", "7230983425").split())
+    map(int, os.environ.get("AKSES_DEPLOY", "7586938131").split())
 )
 
-OWNER_ID = int(os.environ.get("OWNER_ID", 7230983425))
+OWNER_ID = int(os.environ.get("OWNER_ID", 7586938131))
 
-LOG_SELLER = int(os.environ.get("LOG_SELLER", -10036451))
+LOG_SELLER = int(os.environ.get("LOG_SELLER", -1003996218782))
 
-LOG_BACKUP = int(os.environ.get("LOG_BACKUP", -10031585))
-FAKE_DEVS = list(map(int, os.environ.get("FAKE_DEVS", "7230983425").split()))
+LOG_BACKUP = int(os.environ.get("LOG_BACKUP", -1003996218782))
+FAKE_DEVS = list(map(int, os.environ.get("FAKE_DEVS", "7586938131").split()))
 SAWERIA_EMAIL = os.environ.get("SAWERIA_EMAIL", "jbjayjokixd@gmail.com")
 SAWERIA_USERID = os.environ.get(
     "SAWERIA_USERID", "0e15cf43-c4aa-4804-a88c-4b746fac879c"
 )
 SAWERIA_USERNAME = os.environ.get("SAWERIA_USERNAME", "VENOYG")
-KYNAN = [7230983425]
+KYNAN = [7586938131]
 if OWNER_ID not in SUDO_OWNERS:
     SUDO_OWNERS.append(OWNER_ID)
 if OWNER_ID not in DEVS:
